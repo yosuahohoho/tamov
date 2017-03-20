26e30cef296b49623744f5886be43fffad580607
yosuahohoho <yosuahohoho@gmail.com>
2017-03-20 22:41:39 +0700

    update tamov_heroku.py

--- tamov_heroku.py
+++ tamov_heroku.py
@@ -187,7 +187,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://tamovbot.herokuapp.com/")
+    bot.set_webhook(url="https://thawing-cove-33377.herokuapp.com/")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
